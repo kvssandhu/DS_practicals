09aea63393a3675a2185b76e3890abc304ca95e3
Karanveer Singh <sandhunoddy@gmail.com>
2019-08-30 13:53:51 +0530

    Added Menu of Insertion and Deletion. Working on Insertion functions

--- linked_list.c
+++ linked_list.c
@@ -1,21 +1,18 @@
+//Exp 6 : To create a single linked list with nodes having information about students i.e. Name, Roll No. and perform creation, insertion, deletion, count, reversal and display operations.
+
 #include<stdio.h>
 #include<malloc.h>
 
-struct student_details {
-    int roll_no;
-    char name[100];
-};
-
-struct node
-{
-    struct student_details *stu_details;
+struct node{
+    int stu_roll_no;
+    char stu_name[100];
     struct node *next;
 
 } *start;
 
-int main()
-{
-    int ch,n,m,pos,i;
+int main(){
+    int ch,n,m,pos,i, stu_roll_no;
+    char stu_name[100];
     start=NULL;
     while(1)
     {
@@ -24,27 +21,31 @@ int main()
         printf("\n 3. Display link list");
         printf("\n 4. Count number of elements in link list");
         printf("\n 5. Reverse the order of link list");
-        printf("\n 6. Quit");
-        printf("\n\n Enter Choice");
+        printf("\n 6. Insert an element in the list");
+        printf("\n 7. Delete an element from the list");
+        printf("\n 8. Quit");
+        printf("\n\n ==================== Enter Choice : \t");
         scanf("%d",&ch);
         switch(ch)
         {
         case 1:
             printf("\nEnter no. of nodes ");
             scanf("%d",&n);
-
+            printf("\n Enter Student Roll no and name \t");
             for(i=0; i<n; i++)
             {
-                printf("Enter element");
-                scanf("%d",&m);
-                create_list(m, "name");
+
+                scanf("%d%s",&stu_roll_no,stu_name);
+                create_list( stu_roll_no, stu_name);
             }
             break;
         case 2: add_at_beg(); break;
         case 3: display(); break;
         case 4: count(); break;
         case 5: rev(); break;
-        case 6: exit(0);
+        case 6: select_insert_choice(); break;
+        case 7: select_delete_choice(); break;
+        case 8: exit(0);
         default: printf("Wrong Choice");
         }
     }
@@ -53,12 +54,11 @@ int main()
     return 0;
 }
 
-int create_list(int stu_roll_no, char stu_name[100])
-{
+int create_list(int stu_roll_no, char stu_name[]){
     struct node *node,*new_node;
     new_node = (struct node *) malloc(sizeof(struct node));
-    new_node -> stu_details -> roll_no = stu_roll_no;
-   // new_node -> stu_details -> name = stu_name ;
+    new_node -> stu_roll_no = stu_roll_no;
+    strcpy(new_node -> stu_name,stu_name);
     new_node->next=NULL;
     if(start==NULL)
         start=new_node;
@@ -72,8 +72,7 @@ int create_list(int stu_roll_no, char stu_name[100])
     return 0;
 }
 
-int display()
-{
+int display(){
     struct node *node;
     if(start == NULL)
     {
@@ -81,10 +80,10 @@ int display()
         return;
     }
     node=start;
-    printf("\n List is ");
+    printf("\n \t Roll No \t | \t Student Name ");
     while(node!=NULL)
     {
-        printf("%d \t", node -> stu_details -> roll_no);
+        printf(" \n \t %d \t \t \t %s ",node->stu_roll_no, node->stu_name);
         node=node->next;
     }
     printf("\n");
@@ -107,17 +106,16 @@ while(node!=NULL){
     count++;
 }
 
-printf("\n The number of elements in the list are %d", count);
+printf("\n The number of student records in the list are %d \n", count);
 return 0;
 }
 
 int rev(){
 
-printf("Reverse list func called");
 
 struct node *p1, *p2, *p3;
 
-if( start -> next == NULL ) return 0;
+if( start->next == NULL) return 0;
 
 p1 = start; p2 = p1 -> next; p3 = p2 -> next;
 
@@ -133,8 +131,85 @@ while (p3 != NULL){
 
 start = p2;
 
+printf("\n Reversing of the linked list is succesfull. \t");
+
 return 0;
 }
 
+int select_insert_choice(){
+    int ch, stu_roll_no;
+    char stu_name[100];
+    printf("\n Enter Student Roll no and name \t");
+    scanf("%d%s",&stu_roll_no,stu_name);
 
+    printf("\n Select the type of insertion : ");
+    printf("\n 1. Insertion at the beginning \n 2. Insertion at the End \n 3. Insertion at specified location. \n");
+    scanf("%d", &ch);
+    switch(ch){
+        case 1 : insert_at_beginning(stu_roll_no,stu_name); break;
+        case 2 : insert_at_end(stu_roll_no,stu_name); break;
+        case 3 : insert_at_specified_location(stu_roll_no,stu_name); break;
+        default : printf("\n Wrong Choice Entered \n");
+    }
+return 0;
+}
+
+int insert_at_beginning( int stu_roll_no, char stu_name[100] ){
+    struct node *new_node;
+    new_node = (struct node *) malloc(sizeof(struct node));
+    new_node -> stu_roll_no = stu_roll_no;
+    strcpy(new_node -> stu_name,stu_name);
+    new_node -> next = start;
+    start = new_node;
+    printf("\n Element succesfully inserted at the beginning. \n");
+    return 0;
+}
+
+int insert_at_end( int stu_roll_no, char stu_name[100] ){
+    struct node *new_node;
+    new_node = (struct node *) malloc(sizeof(struct node));
+    new_node -> stu_roll_no = stu_roll_no;
+    strcpy(new_node -> stu_name,stu_name);
+    new_node -> next = NULL;
+    //Go to last node , last node ke *next m new node aayega. new node ke next m NUll.
+
+    node -> next = new_node;
+
+    printf("\n Element succesfully inserted at the end. \n");
+    return 0;
+}
+
+int insert_at_specified_location( int stu_roll_no, char stu_name[100] ){
+    struct node *new_node;
+    new_node = (struct node *) malloc(sizeof(struct node));
+    new_node -> stu_roll_no = stu_roll_no;
+    strcpy(new_node -> stu_name,stu_name);
+    return 0;
+}
+
+int select_delete_choice(){
+    int ch;
+    printf("\n Select the type of Deletion : ");
+    printf("\n 1. Delete from the beginning \n 2. Delete from the End \n 3. Delete from specified location. \n");
+    scanf("%d", &ch);
+    switch(ch){
+        case 1 : delete_from_beginning(); break;
+        case 2 : delete_from_end(); break;
+        case 3 : delete_from_specified_location(); break;
+        default : printf("\n Wrong Choice Entered \n");
+    }
+    return 0;
+}
+
+int delete_from_beginning(){
+return 0;
+}
+
+int delete_from_end(){
+return 0;
+}
+
+int delete_from_specified_location(){
+return 0;
+}
 
